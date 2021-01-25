a03358d52529246787b55c5abdb83d5359e9ab67
BartBosmans <bartbosmans.b@gmail.com>
2021-01-25 14:31:15 +0100

    update 10:58

---
+++ main.js
@@ -0,0 +1,20 @@
+// COMMENT
+
+// console.log('success');
+
+/* Multi
+  Line
+  Comment
+*/
+
+// Variables
+
+// var color = 'red';
+// console.log('red');
+
+let age;
+
+age = 30;
+age = 40;
+
+console.log(age);
