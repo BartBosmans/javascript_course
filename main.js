acbd191eb9c65684258d2b4574bd970eb25c24ab
BartBosmans <bartbosmans.b@gmail.com>
2021-01-30 10:44:37 +0100

    update 30-02-2021

--- main.js
+++ main.js
@@ -8,13 +8,53 @@
 */
 
 // Variables
+// Are case-sensitive
+// Should be meaningful and descriptive
+// cannot start with a number
+// cannot contain a space or hyphen (-)
+// cannot be a reserved word
+
 
 // var color = 'red';
 // console.log('red');
 
-let age;
+// let age;
 
-age = 30;
-age = 40;
+// age = 30;
+// age = 40;
 
-console.log(age);
+// let properCamelCase = true;
+
+// let a = 1;
+// let b = 2;
+// let c = 3;
+
+// console.log(age);
+
+// Constants
+
+// const pi = 3.14;
+
+// console.log(pi);
+
+let name = 'John'; // String Literal
+let age = 30 //
+let lovesCats = true; // Boolean Literal
+let color = null; // Null
+let list; // undefined
+
+console.log(name);
+
+// name = 'Mary';
+name =  name + ' Smith'; //concatenate
+
+console.log(name);
+
+let message = name + ' is ' + age;
+console.log(message);
+
+// Array Literals
+
+const numbers = new Array(1,2,3,4,5,6);
+console.log(numbers);
+console.log(numbers[2]);
